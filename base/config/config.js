bc6a4fb14ecd70235086df26a8fefde6c9acac0b
cristiantrave <cristian.trave@gmail.com>
2016-04-10 13:30:05 +0200

    basic structure MVC Node+Express+Jade+MongoDB

---
+++ base/config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'base'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/base-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'base'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/base-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'base'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/base-production'
+  }
+};
+
+module.exports = config[env];
