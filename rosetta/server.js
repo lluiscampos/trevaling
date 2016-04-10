55ae6b6ffa17fe775c9a228a2982d983729be487
cristiantrave <cristian.trave@gmail.com>
2016-04-10 10:24:27 +0200

    added temporal routes server/client

--- rosetta/server.js
+++ rosetta/server.js
@@ -10,6 +10,26 @@ app.get('/', function(request, response){
 	response.sendFile('index.html')
 });
 
+app.get('/philae', function(request, response){
+
+	// Is this you Philae?
+
+		// Yes?
+		// Connect with database and save position
+		// No?
+		// Send error message
+
+	response.sendFile('index.html')
+});
+
+app.get('/viewer', function(request, response){
+
+	// Show philae position tracking
+	// Get info with json and send to view
+
+	response.sendFile('index.html')
+});
+
 app.listen(3000, function(){
 
 	console.log('listen port 3000')
