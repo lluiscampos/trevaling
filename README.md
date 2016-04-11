f4e24a9b04ee7e98c1530a247b0c3f3c22fcef78
Lluis M. Campos <lluiscampos@gmail.com>
2016-04-11 21:48:03 +0200

    ISSUE #5: Add bower init with milligram css
    Use Milligram CSS framework for the time being. Get bowercopy task for
    grunt so that we can deploy the bower components into www. Set also the
    jshint to actually lint our dummy code.
    
    Add some notes into the README file

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # trevaling
 
+Requirements
+============
+
+Need to install node.js v0.12
+
+Install in Ubuntu:
+$ curl -sL https://deb.nodesource.com/setup_0.12 | sudo -E bash -
+
+Install in Windows:
+????
+
+CLIENT
+======
+
+Install dependencies:
+npm install
+bower install
+
+Build the app:
+grunt bowercopy
+
+Run lint:
+grunt jshint
+
 PHILAE
 ======
 
-Requirements:
-
+Install dependencies:
 apt-get install libboost-test-dev lcov
+npm -g install particle-cli
+
+Compile with gcc
+make
+make tests
+
+Compile for particle:
+particle compile electron app.ino philae.cpp philae.h --saveTo firmware.bin
+
+Flash the device via serial:
+sudo particle flash --serial firmware.bin
--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -8,10 +8,28 @@ module.exports = function(grunt) {
     jshint: {
       // You get to make the name
       // The paths tell JSHint which files to validate
-      files: ['dir1/**/*.js', 'dir2/**/*.js']
+      files: ['www/js/*.js']
+    },
+
+    bowercopy: {
+      options: {
+        srcPrefix: 'bower_components'
+      },
+      scripts: {
+        options: {
+          destPrefix: 'www'
+        },
+        files: {
+          'css/milligram.min.css' : 'milligram/dist/milligram.min.css'
+        }
+      }
     }
+
   });
+
   // Each plugin must be loaded following this pattern
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bowercopy');
+
 
 };
