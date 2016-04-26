610512344016513999c9eb74632dbb6b38c0e9a0
Lluis M. Campos <lluiscampos@gmail.com>
2016-04-26 21:29:28 +0200

    ISSUE #4 Add test and assertion framework
    Use mocha as the test framework and chai the assertion one. Add a test
    case for the public API as a BDD demo. Great fun with chai!

---
+++ comet67p/test/test_api.js
@@ -0,0 +1,44 @@
+
+var should = require('chai').should();
+var request   = require("request");
+
+describe("Comet67P API", function() {
+
+  describe("GET api/viewer", function() {
+
+    var url = "http://localhost:3000/viewer";
+
+    it("returns status 200", function(done) {
+      request(url, function(error, response, body) {
+        response.statusCode.should.equal(200);
+
+        done();
+      });
+    });
+
+    it("returns a json string", function(done) {
+      request(url, function(error, response, body) {
+        response.headers['content-type'].should.contain('application/json');
+        body.should.be.a('string');
+
+        done();
+      });
+    });
+
+    it("trip data is parsable", function(done) {
+      request(url, function(error, response, body) {
+        var data = JSON.parse(body);
+        data.should.be.an('array');
+        data[0].should.exist;
+        data[0].should.contain.all.keys('id', 'title', 'locations');
+        data[0].locations.should.be.an('array');
+        data[0].locations[0].should.exist;
+        data[0].locations[0].should.have.all.keys('time', 'latitud');
+
+        done();
+      });
+    });
+
+  });
+
+});
