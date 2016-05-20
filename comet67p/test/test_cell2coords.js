a5dec478b870765368a07862557c72244dfa6acf
Lluis M. Campos <lluiscampos@gmail.com>
2016-05-20 20:11:57 +0200

    ISSUE #13 Mock OpenSignal API to run tests
    Using nock module to mock http requests. The demo response has been
    taking from a real OpenSignal API response.

--- comet67p/test/test_cell2coords.js
+++ comet67p/test/test_cell2coords.js
@@ -1,6 +1,6 @@
 
 var should  = require('chai').should();
-var request = require("request");
+var nock    = require('nock')
 
 var cell2coords  = require('../src/cell2coords.js');
 
@@ -8,14 +8,25 @@ describe("Cell2Coords module", function() {
 
   describe("Translate cellular info into coordinates", function() {
 
-    it("converts {cid, lac} into {lat,lng}", function(done) {
-      this.timeout(2000);
+    var api_resp = {"apiVersion":"2","CID":"13564400","LAC":"20601","phone_type":"GSM",
+        "SID":null,"network_id":null,"perMinuteCurrent":0,"perMinuteLimit":10,
+        "perMonthCurrent":34,"perMonthLimit":2000,"tower1":{"phone_type":"GSM","psc":0,
+        "cid":13564400,"lac":20601,"est_lat":"59.90981531125","est_lng":"10.77598607375",
+        "est_acc":"236.7925","est_stddev":"0.25","2G":0,"3G":1,"4G":0,"network_id_main":2420,
+        "network_ids_json_out":"NULL"}};
 
-      //TODO: Find a way to mock OpenSignal API!
-      cell2coords({cid: 13564400, lac: 20601}, function(error, ret) {
+    before(function(){
+      nock('http://api.opensignal.com')
+        .get(/v2\/towerinfo\.json\?cid=\d+&lac=\d+&apikey=\w+$/)
+        .reply(200, JSON.stringify(api_resp));
+    });
+
+    it("converts {cid, lac} into {lat,lng}", function(done) {
+
+      cell2coords({cid: 13564400, lac: 20601}, function(error, coords) {
         should.not.exist(error);
-        ret.lat.should.equal('59.90981531125');
-        ret.lng.should.equal('10.77598607375');
+        coords.lat.should.equal('59.90981531125');
+        coords.lng.should.equal('10.77598607375');
 
         done();
       });
