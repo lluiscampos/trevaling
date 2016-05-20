2a553757b5677753cb31ebb590396ad80b8ebc1e
Lluis M. Campos <lluiscampos@gmail.com>
2016-05-20 21:24:46 +0200

    ISSUE #13 Handle the case for unknown identifiers
    Gracefully fail when ids are not found in OpenSignal database.

--- comet67p/src/cell2coords.js
+++ comet67p/src/cell2coords.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 
 var convert = function(params, callback) {
 
-  //TODO: Throw an error if the key is not present
-  var apikey = process.env.OPEN_SIGNAL_API_KEY;
+  var apikey = process.env.OPEN_SIGNAL_API_KEY || 'OPEN_SIGNAL_API_KEY';
 
   var url = 'http://api.opensignal.com/v2/towerinfo.json?cid=' + params.cid + '&lac=' + params.lac +'&apikey=' + apikey;
   request(url, function (error, response, body) {
@@ -17,8 +16,15 @@ var convert = function(params, callback) {
     }
     else {
       var obj = JSON.parse(body);
-      //TODO: Do not asume tower1
-      callback(null, {lat: obj.tower1.est_lat, lng: obj.tower1.est_lng})
+      if (obj.hasOwnProperty('tower1')) {
+        callback(null, {lat: obj.tower1.est_lat, lng: obj.tower1.est_lng})
+      }
+      else if (obj.hasOwnProperty('towers') && obj.towers === "No towers with these identifiers") {
+        callback(new Error(obj.towers))
+      }
+      else {
+        callback(new Error('Unable to parse coordinates information from responose ' + body))
+      }
     }
   })
 };
--- comet67p/test/test_cell2coords.js
+++ comet67p/test/test_cell2coords.js
@@ -8,10 +8,10 @@ describe("Cell2Coords module", function() {
 
   describe("Translate cellular info into coordinates", function() {
 
-    var api_resp = {"apiVersion":"2","CID":"13564400","LAC":"20601","phone_type":"GSM",
+    var api_resp = {"apiVersion":"2","CID":"1234","LAC":"5678","phone_type":"GSM",
         "SID":null,"network_id":null,"perMinuteCurrent":0,"perMinuteLimit":10,
         "perMonthCurrent":34,"perMonthLimit":2000,"tower1":{"phone_type":"GSM","psc":0,
-        "cid":13564400,"lac":20601,"est_lat":"59.90981531125","est_lng":"10.77598607375",
+        "cid":1234,"lac":5678,"est_lat":"12.3456789","est_lng":"98.7654321",
         "est_acc":"236.7925","est_stddev":"0.25","2G":0,"3G":1,"4G":0,"network_id_main":2420,
         "network_ids_json_out":"NULL"}};
 
@@ -23,10 +23,36 @@ describe("Cell2Coords module", function() {
 
     it("converts {cid, lac} into {lat,lng}", function(done) {
 
-      cell2coords({cid: 13564400, lac: 20601}, function(error, coords) {
+      cell2coords({cid: 1234, lac: 5678}, function(error, coords) {
         should.not.exist(error);
-        coords.lat.should.equal('59.90981531125');
-        coords.lng.should.equal('10.77598607375');
+        coords.lat.should.equal('12.3456789');
+        coords.lng.should.equal('98.7654321');
+
+        done();
+      });
+
+    });
+
+  });
+
+  describe("Handles unknown identifiers", function() {
+
+    var api_resp = {"apiVersion":"2","CID":"1234","LAC":"5678","phone_type":"GSM",
+        "SID":null,"network_id":null,"perMinuteCurrent":0,"perMinuteLimit":10,
+        "perMonthCurrent":38,"perMonthLimit":2000,"towers":"No towers with these identifiers"};
+
+    before(function(){
+      nock('http://api.opensignal.com')
+        .get(/v2\/towerinfo\.json\?cid=\d+&lac=\d+&apikey=\w+$/)
+        .reply(200, JSON.stringify(api_resp));
+    });
+
+    it("throws error for uknown identifiers", function(done) {
+
+      cell2coords({cid: 1234, lac: 5678}, function(error, coords) {
+        should.exist(error);
+        error.should.be.an('Error');
+        error.message.should.equal('No towers with these identifiers');
 
         done();
       });
