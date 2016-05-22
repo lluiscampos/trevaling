6c1f3b2bea32b17c40cc85259ad98bb346239166
Lluis M. Campos <lluiscampos@gmail.com>
2016-05-22 23:01:49 +0200

    ISSUE #7 Remove philae route, rewrite viewer one
    The route for inserting data is deprecated as the logic will be totally
    independent.

--- comet67p/src/server.js
+++ comet67p/src/server.js
@@ -4,38 +4,15 @@ var dataman = require('./dataman');
 
 var app = express();
 
-app.get('/philae', function(request, response, next){
-
-  dataman.philae(
-    {
-      "id": 2,
-      "title": "first-trip",
-      "locations": [
-        {
-          "time": "now",
-          "latitud": "sur"
-        }
-      ]
-    },
-    function(err) {
-      if(err){
-        return next({message: err, status: 500});
-      }
-    }
-  );
-
-  response.send(200);
-});
-
 app.get('/viewer', function(request, response) {
-
-  // Show philae position tracking
-  // Get info with json and send to view
-
-  response.setHeader('Content-Type', 'application/json');
-
   dataman.viewer({}, function(err, data) {
-    response.send(JSON.stringify(data, null, 4));
+    if (err) {
+      response.status(500).send(err);
+    }
+    else {
+      response.setHeader('Content-Type', 'application/json')
+      response.send(JSON.stringify(data, null, 4));
+    }
   });
 });
 
