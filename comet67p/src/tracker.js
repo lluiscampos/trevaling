db27bbdb2d08bc439853ff9fb6e0ea35f02b612f
Lluis M. Campos <lluiscampos@gmail.com>
2016-06-09 19:07:02 +0200

    ISSUE #11 Tentative code for phiale tracking
    Basic code using particle js API to log into the cloud and subscribe to
    philae events. Code tested locally.

---
+++ comet67p/src/tracker.js
@@ -0,0 +1,39 @@
+
+require('dotenv').config();
+
+var user = process.env.PARTICLE_LOGIN_USER     || 'PARTICLE_LOGIN_USER';
+var pass = process.env.PARTICLE_LOGIN_PASSWORD || 'PARTICLE_LOGIN_PASSWORD';
+
+var Particle = require('particle-api-js');
+var particle = new Particle();
+
+particle.login({username: user, password: pass}).then(
+  function(data){
+    var token = data.body.access_token;
+
+    particle.listDevices({ auth: token }).then(
+      function(devices){
+        var deviceId = devices.body[0].id;
+
+        particle.getEventStream({deviceId: deviceId, auth: token }).then(function(stream) {
+          console.log("Listening to events...")
+          stream.on('event', function(event) {
+            if (event.name === 'current-position')
+            {
+              console.log("Calling dataman:", event)
+            }
+            console.log("Adding to logger:", event)
+          });
+        });
+
+      },
+      function(err) {
+        console.log('List devices call failed: ', err);
+      }
+    );
+
+  },
+  function(err) {
+    console.log('API call completed on promise fail: ', err);
+  }
+);
