358d230430723e2f93a229d5520425076a838de4
Lluis M. Campos <lluiscampos@gmail.com>
2016-06-18 23:29:06 +0200

    ISSUE #11: Add a placeholder for TODO tests

---
+++ comet67p/test/test_tracker.js
@@ -0,0 +1,14 @@
+
+var should  = require('chai').should();
+var sinon   = require('sinon');
+
+var Particle = require('particle-api-js');
+var particle = new Particle();
+
+var tracker  = require('../src/tracker.js');
+
+describe("Tracker module", function() {
+
+  //TODO: Implement tests
+
+});
