66c0398d32b7e051e2c8458c5d749fd2a05b8b32
Lluis M. Campos <lluiscampos@gmail.com>
2016-07-08 19:23:20 +0200

    ISSUE #28 Use OpenShift server configuration

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 
+/* Get settings from OpenShift */
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 /* Create express application */
 var express = require('express');
 var app = express();
@@ -15,6 +19,6 @@ var www = express.static('rosetta/www');
 
 /* Set and start application */
 app.use('/', [server, www])
-app.listen(8080, function() {
-  console.log('Listening port 8080');
+app.listen(server_port, server_ip_address, function() {
+  console.log('Listening port ' + server_port);
 });
