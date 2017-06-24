7dbe493fc302d3a5e3352b714e1aad89e810ebdc
Lluis M. Campos <lluiscampos@gmail.com>
2017-06-24 23:30:52 +0200

    ISSUE #41 Refactor cell2coords module

--- comet67p/src/cell2coords.js
+++ comet67p/src/cell2coords.js
@@ -3,16 +3,15 @@ var logger = require('./logger').cell2coords;
 
 var request = require('request');
 
+var apikey = process.env.GOOGLE_GEOLOCATION_KEY || 'test'
+var url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + apikey;
+
 var convert_single = function(params, callback) {
 
   if ( (params.cid === undefined) || (params.lac === undefined) ) {
     callback(new Error ("Invalid parameters, object must have keys {cid, lac}"));
     return;
   }
-
-  var apikey = process.env.GOOGLE_GEOLOCATION_KEY || 'test'
-
-  var url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + apikey;
 
   var post_params = {
     "cellTowers": [{
@@ -21,27 +20,7 @@ var convert_single = function(params, callback) {
     }]
   };
 
-  request.post(url, post_params, function (error, response, body) {
-    if (error) {
-      callback(new Error('request returned error ' + error))
-    }
-    else if (response.statusCode != 200) {
-      callback(new Error('API call returned status code ' + response.statusCode))
-    }
-    else {
-      var obj = JSON.parse(body);
-      if (obj.hasOwnProperty('location')) {
-        logger.debug('got location from API', body)
-        callback(null, {lat: obj.location.lat, lng: obj.location.lng})
-      }
-      else if (obj.hasOwnProperty('error')) {
-        callback(new Error(JSON.stringify(obj.error)))
-      }
-      else {
-        callback(new Error('Unknown response ' + JSON.stringify(obj)))
-      }
-    }
-  })
+  _post_request(post_params, callback);
 };
 
 var convert_list = function(params, callback) {
@@ -51,10 +30,6 @@ var convert_list = function(params, callback) {
     /* TODO: check for array contents also */
     return;
   }
-
-  var apikey = process.env.GOOGLE_GEOLOCATION_KEY || 'test'
-
-  var url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + apikey;
 
   var cell_towers = [];
 
@@ -73,6 +48,11 @@ var convert_list = function(params, callback) {
     "cellTowers": cell_towers
   };
 
+  _post_request(post_params, callback);
+};
+
+var _post_request = function(post_params, callback)
+{
   logger.debug("POST", post_params);
 
   request.post(url, post_params, function (error, response, body) {
